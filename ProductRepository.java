21a9e2fbf18daade81c1912a7735acbae4fca139
himanshu104 <93647684+himanshu104@users.noreply.github.com>
2021-12-10 04:11:24 -0800

    Update ProductRepository.java

--- ProductRepository.java
+++ ProductRepository.java
@@ -2,7 +2,7 @@ package com.himanshu.springweb.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.priya.springweb.entities.Product;
+import com.himanshu.springweb.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
